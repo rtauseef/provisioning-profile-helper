94b8b8c9e4ffb336c930188cd6ed1a39dfd28d68
Chrissy Harris <chrissy@hp.com>
2017-01-30 10:45:07 -0800

    [sprocket] Adding push notification profile

--- update.rb
+++ update.rb
@@ -6,6 +6,10 @@ current_profile = 'hp.wildcard.mobileprovision'
 current_uuid = uuid = `egrep -a -A2 UUID #{current_profile}`.match(/<string>(.+)<\/string>/).captures.first rescue nil
 current_filename = "#{pp_dir}/#{current_uuid}.mobileprovision"
 
+sprocket_profile = 'Sprocket_Dev_Push_Notification_Profile.mobileprovision'
+sprocket_uuid = uuid = `egrep -a -A2 UUID #{sprocket_profile}`.match(/<string>(.+)<\/string>/).captures.first rescue nil
+sprocket_filename = "#{pp_dir}/#{sprocket_uuid}.mobileprovision"
+
 puts "\n*******************"
 
 found = false
@@ -24,8 +28,18 @@ puts "\nNo old provisioning profiles were found" unless found
 if File.exists?(current_filename)
 	puts "\nCurrent provisioning profile already installed:  #{File.basename(current_filename)}"
 else
+	print "\nCurrent provisioning profile NOT already installed:  #{File.basename(current_filename)}"
 	print "\nCopying #{File.basename(current_filename)}  ⇒  #{pp_dir}... "
 	FileUtils.cp current_profile, "#{current_filename}" 
+	puts 'OK'
+end
+
+if File.exists?(sprocket_filename)
+	puts "\nSprocket provisioning profile already installed:  #{File.basename(sprocket_filename)}"
+else
+	print "\nSprocket provisioning profile NOT already installed:  #{File.basename(sprocket_filename)}"
+	print "\nCopying #{File.basename(sprocket_filename)}  ⇒  #{pp_dir}... "
+	FileUtils.cp sprocket_profile, "#{sprocket_filename}" 
 	puts 'OK'
 end
 
